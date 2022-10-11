c5b6f0cd110c5092b8f3ddb844799e8e81323df1
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-11 11:58:01 +0330

    added search icon to search component and display style

--- src/component/search/Search.jsx
+++ src/component/search/Search.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { ReactComponent as SearchIcon} from "../../assets/svg/search_icon.svg";
+import "./search.style.css";
 
 const Search = () => {
     return(
-        <div>
-            <input type="search"/>
+        <div className="container_search">
+            <div className="box_icon"> 
+                <SearchIcon/>
+            </div>
+            <div>
+                <input type="search" className="search_input"/>
+            </div>
         </div>
     )
 }
--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "../../component/card/Card";
+import Search from "../../component/search/Search";
 
 const Home = ()=>{
     return(
         <div>
+            <Search/>
             <Card/>
         </div>
     )
