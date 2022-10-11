ea5e1e4af13199dfac1ec6d0b7bcebb3a80e54aa
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-11 09:45:52 +0330

    create file database and add data

---
+++ src/database/database.jsx
@@ -0,0 +1,6 @@
+export const todos = [
+    {id:1, title:"buy milk" , description:"Description"},
+    {id:2, title:"buy milk" , description:"Description"},
+    {id:3, title:"buy milk" , description:"Description"},
+    {id:4, title:"buy milk" , description:"Description"},
+]
