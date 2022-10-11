b741354b93e3c8469fc9eff979ecd94154c2a397
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-11 14:05:21 +0330

    added buttens to search component and style to buttons

--- src/component/search/Search.jsx
+++ src/component/search/Search.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import searchIcon from "../../assets/svg/search_icon.svg";
 import "./search.style.css";
+import Button from "../button/Button";
 
 const Search = () => {
+    const filterBtn = [{id:1, title:"All"},{id:2, title:"To DO"},{id:3, title:"Not To DO"}]
     return(
-        <div className="container_search">
-            <div className="box_icon"> 
-                <img src={searchIcon} style={{width:"25px" , height:"25px"}}/>
+        <div className="search">
+            <div className="container_search">
+                <div className="box_icon"> 
+                    <img src={searchIcon} style={{width:"25px" , height:"25px"}}/>
+                </div>
+                <div>
+                    <input type="search" className="search_input"/>
+                </div>
+                <button className="btn_add">Add to do</button>
             </div>
-            <div>
-                <input type="search" className="search_input"/>
+            <div className="filter_btn">
+                {filterBtn.map(item => (
+                    <Button key={item.id}>
+                        {item.title}
+                    </Button>
+                ))}
             </div>
+            
         </div>
     )
 }
