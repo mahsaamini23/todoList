585a50d28003ddd523463c5c27ae2a47e8433025
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-11 14:10:19 +0330

    import Form component to hompage

--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "../../component/card/Card";
 import Search from "../../component/search/Search";
+import Form from "../../component/form/Form";
 
 const Home = ()=>{
     return(
         <div>
             <Search/>
+            <Form/>
             <Card/>
         </div>
     )
