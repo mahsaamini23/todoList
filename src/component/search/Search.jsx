f4c81c49dff039256705878dbb9c31ecd3eb674d
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-11 13:33:49 +0330

    added style to search component

--- src/component/search/Search.jsx
+++ src/component/search/Search.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ReactComponent as SearchIcon} from "../../assets/svg/search_icon.svg";
+import searchIcon from "../../assets/svg/search_icon.svg";
 import "./search.style.css";
 
 const Search = () => {
     return(
         <div className="container_search">
             <div className="box_icon"> 
-                <SearchIcon/>
+                <img src={searchIcon} style={{width:"25px" , height:"25px"}}/>
             </div>
             <div>
                 <input type="search" className="search_input"/>
